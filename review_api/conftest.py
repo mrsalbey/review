c6bbae6a54a741db878d264980c0eab3507f67d4
mrsalbey <41788352+mrsalbey@users.noreply.github.com>
2025-04-22 20:13:11 +0300

    Remove old project

--- review_api/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-# Ensures pytest waits for the database to load
-# https://pytest-django.readthedocs.io/en/latest/faq.html#how-can-i-give-database-access-to-all-my-tests-without-the-django-db-marker
-@pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
-    pass
